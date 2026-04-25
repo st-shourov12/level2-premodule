32b37ac94e2f2f2fd2db2843a99fcd3eaddba44f
st-shourov12 <shourovsc38@gmail.com>
2026-04-25 15:19:45 +0600

    checking performance time

--- Pre-module-1/index.js
+++ Pre-module-1/index.js
@@ -1,4 +1,7 @@
+
+const startTime = performance.now();
 const arr = [1,1,2,3,4,5,2,6,3,5];
 const set = new Set(arr);
 const arr2 = Array.from(set)
-console.log(arr2);
+const endTime = performance.now()
+console.log(`The performance time ${endTime - startTime} ms`);
