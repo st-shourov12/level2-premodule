ddabf17cebc9de6cbdf2810a9ae54acfc839cb6b
st-shourov12 <shourovsc38@gmail.com>
2026-04-27 23:02:30 +0600

    adding premodule task-2

--- Pre-module-1/index.js
+++ Pre-module-1/index.js
@@ -5,3 +5,12 @@ const set = new Set(arr);
 const arr2 = Array.from(set)
 const endTime = performance.now()
 console.log(`The performance time ${endTime - startTime} ms`);
+
+// task 2
+
+console.time("task2");
+const array = [1,2,3,1,2,5,4,6];
+const set2 = new Set(array);
+const array2 = Array.from(set2);
+console.log(array2);
+console.timeEnd("task2")
--- Pre-module-2/index.js
+++ Pre-module-2/index.js
@@ -1,6 +1,14 @@
 const obj = {
     nextLevel: {courseID : "level-2"},
-    "Programming hero" : {courseID : "level-1"}
+    webDev : {courseID : "level-1"}
 }
 
-console.log(obj.nextLevel);
+console.log(obj["webDev"]);
+
+//  task 2
+const course1 = {name : "Programming hero"}
+const obj2 = {};
+obj2.nextLevel = {courseID :"level-2"};
+obj2[course1]  = {courseID : "level-1"}
+
+ console.log(obj2["[object Object]"]);
